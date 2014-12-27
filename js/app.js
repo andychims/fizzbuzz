ec9cc66771f813a9a8582702595bf53eddbd809c
andysplice <andy@splice.com>
2014-12-26 23:05:33 -0700

    all done2

--- js/app.js
+++ js/app.js
@@ -6,17 +6,14 @@ $(document).ready(function() {
 
 	//count to 100
 	for (var i = 1; i <= 100; i++) {
-		console.log(i);
+		var out = i;
 
-//if it's divisible by 3 (if divide by 3 and there's no remainder), don't print the number, instead say "fizz"
-
-
-//if it's divisible by 5, don't print the number, instead say "buzz"
-
-
-//if it's divisible by 3 and 5, don't print the number, instead say "buzz"
-
+		if (i%3 === 0 && i%5 === 0) {out = "fizzbuzz"} 
+		 	else if (i%3 === 0) {out = "fizz"}
+		 	else if (i%5 === 0) {out = "buzz"}
+		 	else {out = i};
+		console.log(out);
+		$("#main").append(out + "<br>");
 	};
-
 
 });
