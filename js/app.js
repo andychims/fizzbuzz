5e724f36bd996ded85acd2c0135ba17c6e5786c7
andysplice <andy@splice.com>
2014-12-26 22:18:20 -0700

    first real commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	console.log("yo");
+	console.log("yo2");
+
+
+	//count to 100
+	for (var i = 1; i <= 100; i++) {
+		console.log(i);
+
+//if it's divisible by 3 (if divide by 3 and there's no remainder), don't print the number, instead say "fizz"
+
+
+//if it's divisible by 5, don't print the number, instead say "buzz"
+
+
+//if it's divisible by 3 and 5, don't print the number, instead say "buzz"
+
+	};
+
+
+});
